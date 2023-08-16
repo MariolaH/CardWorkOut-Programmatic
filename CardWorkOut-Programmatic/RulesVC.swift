2171d79dbde8c6b5a73418cc43993c9623b4257f
Mariola <mariolahullings@gmail.com>
2023-08-15 21:44:42 -0400

    Setup functionality of Rules button

--- CardWorkOut-Programmatic/RulesVC.swift
+++ CardWorkOut-Programmatic/RulesVC.swift
@@ -18,6 +18,7 @@ class RulesVC: UIViewController {
         view.backgroundColor = .systemBackground
         configureTitleLabel()
         configureRuleslabel()
+        configureExerciseLabel()
     }
     
     func configureTitleLabel() {
@@ -37,6 +38,30 @@ class RulesVC: UIViewController {
     func configureRuleslabel() {
         view.addSubview(rulesLabel)
         rulesLabel.translatesAutoresizingMaskIntoConstraints = false
-        rulesLabel.text = "The value of each card represents the number of exercise you do \n\nJ = 11, Q = 12, K = 13, A = 14"
+        rulesLabel.text = "The value of each card represents the number of exercise you do. \n\nJ = 11, Q = 12, K = 13, A = 14"
+        rulesLabel.textAlignment = .center
+        rulesLabel.font = .systemFont(ofSize: 19, weight: .semibold)
+        rulesLabel.lineBreakMode = .byWordWrapping
+        rulesLabel.numberOfLines = 0
+        
+        NSLayoutConstraint.activate([
+            rulesLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50),
+            rulesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            rulesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
+    
+    func configureExerciseLabel() {
+        view.addSubview(exerciseLabel)
+        exerciseLabel.translatesAutoresizingMaskIntoConstraints = false
+        exerciseLabel.text = "♠️ = Push-ups\n\n❤️ = Sit-ups\n\n♣️ = Burpees\n\n♦️ = Jumping Jacks"
+        exerciseLabel.font = .systemFont(ofSize: 19, weight: .semibold)
+        exerciseLabel.numberOfLines = 0
+        
+        NSLayoutConstraint.activate([
+            exerciseLabel.topAnchor.constraint(equalTo: rulesLabel.bottomAnchor, constant: 60),
+            exerciseLabel.widthAnchor.constraint(equalToConstant: 200),
+            exerciseLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 }
